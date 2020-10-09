b957a09577d631b1615f2fac73a1754a47389c0d
BenjiHarper1 <thebenjiharper@gmail.com>
2020-10-09 15:40:31 +0000

    Tried different expressions

--- index.java
+++ index.java
@@ -1,7 +1,8 @@
 void setup() {
 	size(500, 500);
 }
-
+double x = 125;
+double y = 375;
 void draw() {
 // Step 1: Write code that draws the screenshot in the files 
 // with hard-coded values. (Feel free to use colors 
@@ -15,11 +16,17 @@ void draw() {
 // For example, “variable1 = variable1 + 2;”. 
 // Try different expressions and see what happens!
 
+x = x - 0.1;
+y = y / 1.5;
+
   background(255);
-  stroke(0);
-  fill(175);
+  stroke(x,y,x);
+  x = x + 5;
+  fill(y,x,255);
+ellipse(x,x,x,x);
+ellipse(y,x,x,x);
+ellipse(y,y,x,x);
+ellipse(x,y,x,x);
 
-
-  
  
 }
